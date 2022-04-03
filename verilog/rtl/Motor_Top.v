47617a4cfb40aa6f7537858a7c185e3c0eefc164
Ali Imran <ali112000@pop-os.localdomain>
2022-04-03 05:12:52 +0500

    Verilog files Updated

--- verilog/rtl/Motor_Top.v
+++ verilog/rtl/Motor_Top.v
@@ -1,3 +1,19 @@
+// SPDX-FileCopyrightText: 2022 EE, UET Lahore
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileContributor: Created by Muhammad Tahir <mtahir@uet.edu.pk>
+
 module Interlink_Module( // @[:@4045.2]
   input         clock, // @[:@4046.4]
   input         reset, // @[:@4047.4]
